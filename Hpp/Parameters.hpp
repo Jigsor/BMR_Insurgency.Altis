03889067506d95cc2a0500b1883660c4f4a1de3a
7rust <luca.online@web.de>
2016-08-29 17:49:57 +0200

    Changed classnames to fit with variable names
    Changed classnames to fit with variable names.
    
    Now you can add a new parameter (without taking care of the order or
    index of paramsArray) and it will be available as variable with the
    parameters classname in INS_definitions.sqf.

--- Hpp/Parameters.hpp
+++ Hpp/Parameters.hpp
@@ -7,7 +7,7 @@ class Params
 	texts[]={ ""};
 	default = 0;
 	};
-	class INS_b_time//1
+	class INS_p_time//1
     {
 	//title = $STR_BMR_start_time;
 	title = "		Set the start time:";
@@ -15,7 +15,7 @@ class Params
 	texts[]={"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24"};
 	default = 12;
 	};
-	class init_JIP_weather//2
+	class JIPweather//2
 	{
 	title = "		Weather";
 	values[]={0,25,50,75,1,2,3};
@@ -29,28 +29,28 @@ class Params
 	"Dynamic Random Weather Enabled"};
 	default = 25;
 	};
-	class radio_chatter//3
+	class ambRadioChatter//3
 	{
 	title = "		Enable Ambient Vehicle Radio Chatter?";
 	values[]={0,1};
 	texts[]={"No","Yes"};
 	default = 0;
 	};
-	class combat_sounds//4
+	class ambCombatSound//4
 	{
 	title = "		Enable Ambient Combat Sounds?";
 	values[]={0,1};
 	texts[]={"No","Yes"};
 	default = 0;
 	};
-	class grass_tog//5
+	class Remove_grass_opt//5
 	{
 	title = "		Grass Option None";
 	values[]={0,1};
 	texts[]={"Enabled","Disabled"};
 	default = 0;
 	};
-	class enviroment_tog//6
+	class INS_environment//6
 	{
 	title = "		Environment effects (ambient life + sound)";
 	values[]={0,1};
@@ -64,7 +64,7 @@ class Params
 	texts[]={ ""};
 	default = 0;
 	};
-	class INS_revive_system//8
+	class INS_p_rev//8
 	{
 	title="		Revive system";
 	values[]={0,1,2,3,4,5,6,7};
@@ -79,7 +79,7 @@ class Params
 	"Revive System Disabled. Mobile HQ Enabled."};
 	default = 1;
 	};
-	class INS_revive_time//9
+	class INS_p_rev_time//9
 	{
 	title="		Revive time";
 	values[]={60,120,180,300,600,1800,3600};
@@ -93,7 +93,7 @@ class Params
 	texts[]={ ""};
 	default = 0;
 	};
-	class INS_enemy_army//11
+	class INS_op_faction//11
 	{
 	title = "		Opposing Army/Mod Initialization";
 	values[]={1,2,3,4,5,6,7,8,9,10,11,12};
@@ -119,56 +119,56 @@ class Params
 	texts[]={ ""};
 	default = 0;
 	};
-	class infantry_chance//13
+	class InfPb//13
 	{
 	title = "		Enemy Infantry Probability";
 	values[]={25,50,75,100};
 	texts[]={"25 % chance","50 % chance","75 % chance","100 % chance"};
 	default = 50;
 	};
-	class armor_chance//14
+	class MecArmPb//14
 	{
 	title = "		Enemy Armor Probability";
 	values[]={1,25,50,75,100};
 	texts[]={"0 % chance Heavy Armor + Rewards disabled","25 % chance","50 % chance","75 % chance","100 % chance"};
 	default = 75;
 	};
-	class AI_spawn_Dis//15
+	class AI_SpawnDis//15
 	{
 	title = "		Enemy AI Spawn Trigger Distance";
 	values[]={200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000,1050,1100,1150,1200,1250};
 	texts[]={"200","250","300","350","400","450","500","550","600","650","700","750","800","850","900","950","1000","1050","1100","1150","1200","1250"};
 	default = 350;
 	};
-	class Act_Zone_Limit//16
+	class Max_Act_Gzones//16
 	{
 	title = "		Maximum Simultaneous Activated Zone Limit";
 	values[]={10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,150,300,1000};
 	texts[]={"10","15","20","25","30","35","40","45","50","55","60","65","70","75","80","85","90","95","100","150","300","1000"};
 	default = 45;
 	};
-	class Deactivate_Zone_Delay//17
+	class DeAct_Gzone_delay//17
 	{
 	title = "		Grid zone deactivation delay";
 	values[]={0,0.5,1,2,3,4,5,6,8,10,12,14,16,18,20,30};
 	texts[]={"Disabled","30 seconds","1 minute","2 minutes","3 minutes","4 minutes","5 minutes","6 minutes","8 minutes","10 minutes","12 minutes","14 minutes","16 minutes","18 minutes","20 minutes","30 minutes"};
 	default = 2;
 	};
-	class Enemy_Air//18
+	class EnableEnemyAir//18
 	{
 	title = "		Enable JIG Enemy Air Patrols?";
 	values[]={0,1,2,3,4,5,6};
 	texts[]={"No","A3 Helis Only","A3 Fixed Wing Only","A3 Helis and A3 Fixed Wing","Moded Helis Only","Moded Fixed Wing Only","Moded Helis and Moded Fixed Wing"};
 	default = 3;
 	};
-	class AiAirRespawn//19
+	class AirRespawnDelay//19
 	{
 	title = "		Minimum Enemy Air Patrol Respawn Delay";
 	values[]={45,300,600,1200,1800,2400,3000,3600};
 	texts[]={"45 seconds","5 minutes","10 minutes","20 minutes","30 minutes","40 minutes","50 minutes","60 minutes"};
 	default = 2400;
 	};
-	class patrolewpmod//20
+	class PatroleWPmode//20
 	{
 	title = "		Air Patrol way-point type";
 	values[]={0,1};
@@ -182,14 +182,14 @@ class Params
 	texts[]={ ""};
 	default = 0;
 	};
-	class BTC_set_ai_skill//22
+	class BTC_p_skill//22
 	{
 	title = "		Set AI skill on non EOS units. (ASR AI detection will override this)";
 	values[]={0,1};
 	texts[]={"No","Yes"};
 	default = 1;
 	};
-	class BTC_AI_accuracy//23
+	class BTC_AI_skill//23
 	{
 	title = "		AI accuracy on non EOS units. (ASR AI detection will override this)";
 	values[]={1,2,3,4,5,6,7,8,9,10};
@@ -203,28 +203,28 @@ class Params
 	texts[]={ ""};
 	default = 0;
 	};
-	class amb_mobile_civs//25
+	class CiviMobiles//25
 	{
 	title = "		Ambient Mobile Civilians?";
 	values[]={0,1};
 	texts[]={"No","Yes"};
 	default = 1;
 	};
-	class amb_foot_civs//26
+	class CiviFoot//26
 	{
 	title = "		Ambient Foot Civilians?";
 	values[]={0,1};
 	texts[]={"No","Yes"};
 	default = 1;
 	};
-	class foot_civs_chance//27
+	class CivProbability//27
 	{
 	title = "		Ambient Foot Civilians Probability";
 	values[]={25,50,75,100};
 	texts[]={"25 % chance","50 % chance","75 % chance","100 % chance"};
 	default = 100;
 	};
-	class sst_bomber//28
+	class SuicideBombers//28
 	{
 	title = "		Enable Civilian Suicide Bomber?";
 	values[]={0,1};
@@ -238,7 +238,7 @@ class Params
 	texts[]={ ""};
 	default = 0;
 	};
-	class INS_playable_opfor//30
+	class INS_play_op4//30
 	{
 	title = "		Playable Opfor";
 	values[]={0,1};
@@ -252,28 +252,28 @@ class Params
 	texts[]={"Disabled","Enabled"};
 	default = 1;
 	};
-	class can_fatigue//32
+	class Fatigue_ability//32
 	{
 	title = "		Fatigue and Stamina System";
 	values[]={0,1};
 	texts[]={"Arma 3 Player Fatigue and Stamina Disabled","Arma 3 Default Player Fatigue and Stamina Enabled"};
 	default = 0;
 	};
-	class damage_multiplier//33
+	class EOS_DAMAGE_MULTIPLIER//33
 	{
 	title = "		Damage Multiplier (Effective hit on enemy)";
 	values[]={0.5,1,2,3};
 	texts[]={"Low","Default","High","Very High"};
 	default = 2;
 	};
-	class init_JIG_EX//34
+	class JigHeliExtraction//34
 	{
 	title = "		Enable CAS1 Group Heli Extraction?";
 	values[]={0,1};
 	texts[]={"No","Yes"};
 	default = 1;
 	};
-	class gasnade_mod//35
+	class INS_GasGrenadeMod//35
 	{
 	title = "		Enable Gas Grenades and Masks";
 	values[]={0,1};
@@ -282,28 +282,28 @@ class Params
 	"Yes (Yellow Hand and GL smoke grenades. A3 Heli Crew Helmets and or Gas Masks from @hiddenidentitypack or @nato_russian_sf_weapons mods"};
 	default = 1;
 	};
-	class 3rd_person//36
+	class limitPOV//36
 	{
 	title = "		Third person view in vehicles only?";
 	values[]={0,1};
 	texts[]={"No","Yes"};
 	default = 0;
 	};
-	class INS_Recruitable_AI//37
+	class max_ai_recruits//37
 	{
 	title = "		Recruitable AI units maximum allowed";
 	values[]={1,2,3,4,5,6,7,8,9,10};
 	texts[]={"Recruitable AI disabled","1","2","3","4","5","6","7","8","9"};
 	default = 10;
 	};
-	class Audible_AI_radio//38
+	class AI_radio_volume//38
 	{
 	title = "		Dissable Audible AI Radio?";
 	values[]={0,1};
 	texts[]={"No","Yes"};
 	default = 0;
 	};
-	class full_loadout//39
+	class INS_full_loadout//39
 	{
 	title = "		Enable Save/Restore full loadout on respawn?";
 	values[]={0,1};
@@ -317,21 +317,21 @@ class Params
 	texts[]={ ""};
 	default = 0;
 	};
-	class init_ammo_caches//41
+	class EnemyAmmoCache//41
 	{
 	title = "		Enable Enemy Ammo Caches?";
 	values[]={0,1};
 	texts[]={"No","Yes"};
 	default = 1;
 	};
-	class intel_locations//42
+	class Intel_Loc_Alpha//42
 	{
 	title = "		Show Intel Location Markers?";
 	values[]={0,1};
 	texts[]={"No","Yes"};
 	default = 1;
 	};
-	class intel_ratio//43
+	class Intel_Count//43
 	{
 	title = "		Maximum possible intel per occupied grid zone ratio";
 	values[]={2,3,4,5,6};
@@ -345,7 +345,7 @@ class Params
 	texts[]={ ""};
 	default = 0;
 	};
-	class INS_editor_debug//45
+	class DebugEnabled//45
 	{
 	title = "		Debug mode?";
 	values[]={0,1};
